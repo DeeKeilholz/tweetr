290f426e831035ab881614274e624f9f4aa2c320
DeeKeilholz <deekeilholz@gmail.com>
2016-11-16 19:07:47 +0000

    tweet form set up and hover works

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,7 +5,35 @@
  */
 $(document).ready(function() {
 
-  var $container = $('#all-tweets');
+  // var $container = $('#all-tweets');
+//tweets is an empty array when JS loads
+  var tweets = []
+
+  function renderTweets () {
+    let $el = $('#all-tweets');
+    //creates a string that will be html we insert
+    let html = "";
+    // loop through tweets function for each instance append string to html
+    // string, and append name of tweet
+    tweets.forEach((tweet)) => {
+      html += <li><a href="#">$(tweet.name)</a></li>
+    })
+    // append call
+    $el.append(html)
+  }
+
+  $(document).ready(() => {
+
+    $.ajax('/tweets', {
+      method: 'GET'
+    }).done((data) =>
+    tweets = data
+    renderTweets()
+  })
+
+
+
+
 
   for(tweet of tweets){
   	var $tweet = $("<div>").addClass("tweet")
