3686176bfa97c77d30fdd5e2b386743121e518bb
DeeKeilholz <deekeilholz@gmail.com>
2016-11-17 21:52:55 +0000

    add app.js file with ajax GET and POST

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,46 +3,99 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-$(document).ready(function() {
 
-  // var $container = $('#all-tweets');
-//tweets is an empty array when JS loads
-  var tweets = []
+ $(document).ready(() => {
 
-  function renderTweets () {
-    let $el = $('#all-tweets');
-    //creates a string that will be html we insert
-    let html = "";
-    // loop through tweets function for each instance append string to html
-    // string, and append name of tweet
-    tweets.forEach((tweet)) => {
-      html += <li><a href="#">$(tweet.name)</a></li>
-    })
-    // append call
-    $el.append(html)
-  }
+const renderTweets = (tweet) => {
+  //  function joins the converted tweet data into one continuous html string
+  //   and attaches it to the page
+  let htmlString = '';
+  tweet.forEach((elm) => {
+  htmlString = createTweetElement(elm) + htmlString;
+  });
+// emptying pervious tweeter string before adding new one.
+  $('#all-tweets').empty();
+  let $tweets = $('#all-tweets').prepend(htmlString);
+  return $tweets;
+}
 
-  $(document).ready(() => {
 
-    $.ajax('/tweets', {
-      method: 'GET'
-    }).done((data) =>
-    tweets = data
-    renderTweets()
+const createTweetElement = (tweetobj) => {
+  const html_data =
+    `<article class="tweets-article">
+       <header>
+         <div class="header-content">
+           <img class="avatarclass" src="${tweetobj.user.avatars.small}">
+           <h2 class="username">${tweetobj.user.name}</h2>
+           <h5 class="handle">${tweetobj.user.handle}</h5>
+        </div>
+      </header>
+
+        <div class="message">${tweetobj.content.text}</div>
+
+      <footer>
+        <p>${tweetobj.created_at} days ago</p>
+
+         <div class="footer-icons">
+
+           <span class="icons-rows">
+             <i class="fa fa-flag fa-lg"></i>
+             <i class="fa fa-coffee fa-lg"></i>
+             <i class="fa fa-heart fa-lg"></i>
+           </span>
+
+         </div>
+
+      </footer>
+
+  </article>`
+
+  return html_data;
+}
+
+
+
+// function called loadTweets that is responsible for fetching tweets from /tweets
+
+const loadTweets = () => {
+
+$.ajax({
+  //uses jQuery to make a request to /tweets
+  method: 'GET',
+  url: '/tweets',
+  // receives the array of tweets as JSON
+  dataType: 'json',
+  success: (tweet) => {
+    //calling up the renderTweets function b/c it takes in the array of objexts
+    //and renders them to the DOM
+    renderTweets(tweet);
+    }
   })
+}
+
+loadTweets()
 
 
+const tweetSubmit = () => {
+  // process my form
+  $("new-tweet-form").submit(function(event) {
+    event.preventDefault();
+    // get the form data using form id
+    const textInput = $('#textarea').val();
+    //process the form with ajax
+      $.ajax({
+        method: 'POST', // defines the type of HTTP verb I want to use
+        url: "/tweets", // the url where I want to post
+        data: $('new-tweet-form').serialize(), // my data object - serilize function acts on my
+        // object and creates a text string in standard URL-encoded notation
+        success: (tweet) => {
+          loadTweets();
+        }
+      })
+  });
+}
 
-
-
-  for(tweet of tweets){
-  	var $tweet = $("<div>").addClass("tweet")
-     .append($("<div>").text(tweet.article-header))
-     .append($("<div>").text(tweet.article-body))
-     .append($("<div>").text(tweet.article-footer));
-
-    $container.append($tweet);
-  }
+tweetSubmit(); // calling my function
 
 
 });
--- server/index.js
+++ server/index.js
@@ -2,14 +2,17 @@
 
 const PORT        = 8080;
 const express     = require("express");
+const morgan = require('morgan');
 const bodyParser  = require("body-parser");
 const app         = express();
+
 
 const tweetsApi  = require('./api/tweets');
 const db         = require('./lib/db');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(morgan('dev'));
 
 db.connect((dbInstance) => {
   app.use('/tweets', tweetsApi(dbInstance));
